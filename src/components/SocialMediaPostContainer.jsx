ece56014aa44223ead03bcae0fd6f969cb1888cb
John Niduelan <jeniduelan@gmail.com>
2024-09-19 19:43:42 +1000

    feat: component structures started

---
+++ src/components/SocialMediaPostContainer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default class SocialMediaPostContainer extends React.Component {
+    constructor(){
+        super();
+
+        this. state = {
+            isEditing: false,
+            author: "",
+            dateCreated: Date.now(),
+            lastUpdated: Date.now(),
+            location: "",
+            content: "",
+        }
+    }
+
+    render(){
+
+		this.props.parentState.someProperty 
+
+		if (this.state.isEditing){
+			return(
+				<EditablePost 
+					author={this.state.author} 
+					dateCreated={this.state.dateCreated} 
+					lastUpdated={this.state.lastUpdated} 
+					location={this.state.location}
+					content={this.state.content}
+
+					parentState={this.state}
+				/>
+			);
+		} else {
+			return(
+				<ReadonlyPost 
+					author={this.state.author} 
+					dateCreated={this.state.dateCreated} 
+					lastUpdated={this.state.lastUpdated} 
+					location={this.state.location}
+					content={this.state.content}
+
+					parentState={this.state}
+				/>
+			);
+		}
+	}
+}
